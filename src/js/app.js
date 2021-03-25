238218be1596af088a82e4a061fed1dc81474661
Lina Kastl <lina_ka@gmx.de>
2021-03-25 18:20:23 +0100

    starts shop ands cart

--- src/js/app.js
+++ src/js/app.js
@@ -1,4 +1,5 @@
 // variables
+import defaultProducts from "./products.json";
 
 const cartBtn = document.querySelector(".cart-btn")
 const closeCartBtn = document.querySelector(".close-cart")
@@ -7,7 +8,7 @@ const cartOverlay = document.querySelector(".cart-overlay")
 const cartItems = document.querySelector(".cart-items")
 const cartTotal = document.querySelector(".cart-total")
 const cartContent = document.querySelector(".cart-content")
-const productsDOM = document.querySelector(".products")
+const productsDOM = document.querySelector(".all-products")
 
 
 // cart
@@ -20,15 +21,17 @@ let cart = [];
 class Products {
     async getProducts() {
         try {
-            let result = await fetch("./products.json");
-            let data = await result.json();
-
-            let products = data.items;
+            let products = defaultProducts.items;
             products = products.map(item=>{
-                const {productName,weight,price} = item.variants;
-                const {id} = item.id; 
+                const {id, productName, variants} = item; 
                 const image = item.image.fields.file.url;
-                return {productName,weight,price,id,image}
+                return {
+                    productName,
+                    weight: variants[0].weight,
+                    price: variants[0].price,
+                    id,
+                    image
+                }
             })
             return products
         } catch (error) {
@@ -42,10 +45,29 @@ class Products {
 
 class UI {
     displayProducts(products) {
+        console.log(products);
         let result = '';
         products.forEach(product => {
-            result += ``
-        })
+            result += `
+            <article class="product">
+                <div class="product-img-container">
+                  <img
+                    class="product-pic"
+                    src=${product.image}
+                    alt="product coffee from costa rica"
+                  />
+                  <div class="split-btn">
+                    <button class="btn-250" data-id=${product.id}>250g</button>
+                    <button class="btn-500" data-id="0">500g</button>
+                    <button class="btn-1000" data-id="0">1.000g</button>
+                  </div>
+                </div>
+                <p class="product-name dark">${product.productName}</p>
+                <p class="product-price">6,90€ – 26,90€</p>
+              </article>
+            `
+        });
+        productsDOM.innerHTML = result;
     }
 }
 
@@ -58,10 +80,10 @@ class Storage {
 
 // event listener
 
-document.addEventListener("SOMContentLoaded", () => {
+//document.addEventListener("DOMContentLoaded", () => {
     const ui = new UI();
     const products = new Products ();
 
     // get all producs
     products.getProducts().then(products => ui.displayProducts (products))
-});
+//});
